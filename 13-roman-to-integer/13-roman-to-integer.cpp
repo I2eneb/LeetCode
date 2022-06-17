af40787ee59f80c3ee31add3969e10a5634e3e45
i2eneb <44600728+I2eneb@users.noreply.github.com>
2022-06-18 01:15:35 +0900

    Time: 32 ms (15.52%), Space: 11.5 MB (5.03%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -3,14 +3,14 @@ public:
     int romanToInt(string s) {
     int result = 0;
 
-    map <string, int> symbols;
-    symbols.insert({ "I", 1 });
-    symbols.insert({ "V", 5 });
-    symbols.insert({ "X", 10 });
-    symbols.insert({ "L", 50 });
-    symbols.insert({ "C", 100 });
-    symbols.insert({ "D", 500 });
-    symbols.insert({ "M", 1000 });
+    map <char, int> symbols;
+    symbols.insert({ 'I', 1 });
+    symbols.insert({ 'V', 5 });
+    symbols.insert({ 'X', 10});
+    symbols.insert({ 'L', 50});
+    symbols.insert({ 'C', 100});
+    symbols.insert({ 'D', 500});
+    symbols.insert({ 'M', 1000});
 
     map <string, int> symbolsComp;
     symbolsComp.insert({ "IV", 4 });
@@ -20,11 +20,9 @@ public:
     symbolsComp.insert({ "CD", 400 });
     symbolsComp.insert({ "CM", 900 });
 
+    string key;
     for (auto iter = symbolsComp.begin(); iter != symbolsComp.end(); iter++) {
-        if (s == "")
-            break;
-
-        string key = iter->first;
+        key = iter->first;
         int index = s.find(key);
         if (index == string::npos)
             continue;
@@ -33,25 +31,15 @@ public:
 
         s.erase(index, key.length());
     }
-        
-    for (auto iter = symbols.begin(); iter != symbols.end(); iter++) {
-        if (s == "")
-            break;
 
-        string key = iter->first;
-        for (size_t i = 0; i < s.length(); i++) {
-            int index = s.find(key);
-            if (index == string::npos)
-                break;
-            else {
+    for (auto iter = symbols.begin(); iter != symbols.end(); iter++) {
+        for (auto iter_str = s.begin(); iter_str != s.end(); iter_str++) {
+            if (iter->first == *iter_str) {
                 result += iter->second;
-                s.erase(index, key.length());
-                i--;
             }
         }
     }
 
     return result;
 }
-
 };
