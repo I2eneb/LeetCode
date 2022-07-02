57b621bdca17c445f48eaad0cb6fe2a9f9e11ab9
i2eneb <44600728+I2eneb@users.noreply.github.com>
2022-07-02 23:09:22 +0900

    Time: 11 ms (28.00%), Space: 7.7 MB (86.46%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        
-        int wealth = 0;
         int wealthMax = 0;
         
         for(auto i = 0; i < accounts.size(); i++) {
+            int wealth = 0;
             for(auto j = 0; j < accounts[i].size(); j++) {
                 wealth += accounts[i][j];
             }
-            
-            if (wealth > wealthMax)
-                wealthMax = wealth;
-
-            wealth = 0;
+            wealthMax = max(wealth,wealthMax);
         }
         
         return wealthMax;
